222c779a56a240b933d5ea209a83565806b5b915
Jason Brown <browniefed@gmail.com>
2017-08-19 15:43:48 -0700

    Add in tests/snaps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const Sidebar = ({ children }) => {
+  return (
+    <div className="sidebar">
+      {children}
+    </div>
+  );
+};
+
+const Content = ({ children }) => {
+  return (
+    <div className="content">
+      {children}
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Sidebar>
+          <div>Fancy Sidebar</div>
+        </Sidebar>
+        <Content>
+          <div>Hello</div>
+        </Content>
       </div>
     );
   }
