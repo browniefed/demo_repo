caf1975d5c78f4cf32681ac4ea9a90f79c7313c8
Jason Brown <browniefed@gmail.com>
2017-08-21 18:06:54 -0700

    Clear stuff

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const Sidebar = ({ children }) => {
-  return (
-    <div className="sidebar">
-      {children}
-    </div>
-  );
-};
-
-const Content = ({ children }) => {
-  return (
-    <div className="content">
-      {children}
-    </div>
-  );
-};
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sidebar>
-          <div>Fancy Sidebar</div>
-        </Sidebar>
-        <Content>
-          <div>Hello</div>
-        </Content>
+        
       </div>
     );
   }
