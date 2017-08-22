b7b22981ce72294098ab975092021802451ad191
Jason Brown <browniefed@gmail.com>
2017-08-21 18:28:27 -0700

    Add notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// Life Cycle Events
+// Higher Order Components
+// Selectors
+
 class App extends Component {
   render() {
     return (
