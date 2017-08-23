fa3d8465f9ca84de50928718d3e734be36141562
Jason Brown <browniefed@gmail.com>
2017-08-22 21:23:50 -0700

    Add admin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Admin from "./admin";
+
 class App extends Component {
+  state = {
+    list: [],
+  };
+  handleAddItem = item => {
+    this.setState(state => {
+      state.counter == 0;
+      return {
+        list: [...state.list, item],
+      };
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        
+        <Admin 
+          list={this.state.list}
+          onAddItem={this.handleAddItem}
+        />
       </div>
     );
   }
