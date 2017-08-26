6d00486e72d07b650d249891d62962acfd398e3e
Jason Brown <browniefed@gmail.com>
2017-08-26 10:08:10 -0700

    Add basics routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./home";
+import About from "./about"
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+        <div className="App">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { BrowserRouter, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const routes = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById("root"));
