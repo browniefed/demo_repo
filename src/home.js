9c99ce8de4b52ed627a4f7b220ec1eba0c276878
Jason Brown <browniefed@gmail.com>
2017-08-26 15:30:42 -0700

    Add in lab

--- src/home.js
+++ src/home.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 
+import { search } from "./api";
+
 class HomeContainer extends Component {
   state = {
     value: "",
+    gifs: [],
   };
-  handleSearch = (e) => {
+  handleSearch = e => {
     if (!e.key === "Enter") return;
-
-    
-  }
+    search(this.state.value).then(gifs => {
+      this.setState({
+        gifs: gifs.data,
+      });
+    });
+  };
   render() {
     return (
       <div>
-        <input 
-          value={this.state.value} onChange={e => this.setState({ value: e.target.value })}
+        <input
+          value={this.state.value}
+          onChange={e => this.setState({ value: e.target.value })}
           onKeyUp={this.handleSearch}
         />
+        <div>
+          {this.state.gifs.map(gif => {
+            const imgSrc = gif.images.downsized_medium;
+            return <img key={gif.id} src={imgSrc.url} />;
+          })}
+        </div>
       </div>
     );
   }
